2e44b88a8e81596d1ca5002f4d2f282b5adaf66e
xLastNightx <mikhalevich5katya@gmail.com>
2025-11-20 17:34:08 +0300

    now it is more readable

--- lecture_2/main.py
+++ lecture_2/main.py
@@ -10,7 +10,7 @@ current_year = today.year
 user_name = input("Hello, dear! Enter your name: ")
 birth_year = int(input("Enter your birth year: "))
 
-if birth_year > 2025 or birth_year <= 1920: 
+if birth_year > 2025 or birth_year <= 1920:
     trulyBirth = False
     while trulyBirth == False:
         birth = int(input("enter your truly birth year: "))
@@ -21,14 +21,17 @@ if birth_year > 2025 or birth_year <= 1920:
 current_age = current_year - birth_year
 
 # calculating the life_stage
-def generate_profile (age):
+
+
+def generate_profile(age):
     if age <= 12:
         life_stage = "Child"
-    elif age < 19: 
-        life_stage = "Teenager" 
-    else: 
+    elif age < 19:
+        life_stage = "Teenager"
+    else:
         life_stage = "Adult"
     return life_stage
+
 
 stage = generate_profile(current_age)
 
@@ -37,22 +40,26 @@ desire_to_input_another_hobby = True
 
 # adding user hobbies into the dict
 while desire_to_input_another_hobby:
-    hobby = input("Enter your favorite hobby or type 'stop' to finish it: ").lower()
+    hobby = input(
+        "Enter your favorite hobby or type 'stop' to finish it: ").lower()
     if hobby == "stop":
         desire_to_input_another_hobby = False
         break
-    else: 
-        hobbies.add(hobby)  
+    else:
+        hobbies.add(hobby)
 
-user_profile = {"name": user_name, "age": current_age, "stage": stage, "hobbies": hobbies}
+user_profile = {"name": user_name, "age": current_age,
+                "stage": stage, "hobbies": hobbies}
 
 # printing everything in the terminal
-print(f"\nYour name is {user_profile["name"]} and you are {user_profile["age"]} years old, it means, that you are {user_profile["stage"]}.")
-print(f"\nProfile Summary: \nname: {user_profile["name"]} \nage: {user_profile["age"]} \nstage: {user_profile["stage"]} \n")
+print(
+    f"\nYour name is {user_profile["name"]} and you are {user_profile["age"]} years old, it means, that you are {user_profile["stage"]}.")
+print(
+    f"\nProfile Summary: \nname: {user_profile["name"]} \nage: {user_profile["age"]} \nstage: {user_profile["stage"]} \n")
 
 if len(hobbies) == 0:
     print("Your list of hobbies is empty")
-else: 
+else:
     print(f"Your favorite hobbies ({len(hobbies)}): ")
     for item in user_profile["hobbies"]:
         print(f"- {item}")
